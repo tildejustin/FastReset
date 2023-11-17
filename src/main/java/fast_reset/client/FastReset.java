fee3ee55826fe7658917fd3ffc5a60ec2ba6b0b5
contaria <kingcontaria@gmail.com>
2023-11-17 05:33:09 +0100

    remove multi threading it causes the most problems and barely does anything because we wait on it to finish later on anyway

--- src/main/java/fast_reset/client/FastReset.java
+++ src/main/java/fast_reset/client/FastReset.java
@@ -1,15 +1,7 @@
 package fast_reset.client;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class FastReset {
 
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static final AtomicBoolean saving = new AtomicBoolean();
-    public static final Object saveLock = new Object();
     public static FastResetConfig config;
     public static boolean saveOnQuit = true;
 }
--- src/main/java/fast_reset/client/mixin/MinecraftServerMixin.java
+++ src/main/java/fast_reset/client/mixin/MinecraftServerMixin.java
@@ -3,14 +3,11 @@ package fast_reset.client.mixin;
 import fast_reset.client.FastReset;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Iterator;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
@@ -18,32 +15,6 @@ public abstract class MinecraftServerMixin {
     @ModifyConstant(method = "shutdown", constant = @Constant(intValue = 0, ordinal = 0))
     private int disableWorldSaving(int savingDisabled) {
         return FastReset.saveOnQuit ? savingDisabled : 1;
-    }
-
-    @Redirect(method = "shutdown", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", ordinal = 1))
-    private boolean closeWorldsRedirect(Iterator<ServerWorld> iterator) {
-        if (FastReset.saveOnQuit) {
-            return iterator.hasNext();
-        }
-
-        new Thread(() -> {
-            synchronized (FastReset.saveLock) {
-                FastReset.saving.set(true);
-
-                while (iterator.hasNext()) {
-                    ServerWorld world = iterator.next();
-                    if (world != null) {
-                        try {
-                            world.close();
-                        } catch (Exception ignored) {
-                        }
-                    }
-                }
-
-                FastReset.saving.set(false);
-            }
-        }).start();
-        return false;
     }
 
     @Redirect(method = "shutdown", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;saveAllPlayerData()V"))
