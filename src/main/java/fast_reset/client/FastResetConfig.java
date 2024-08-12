31c2a007f79766c59eecb6c4352582ce18ef0667
tildejustin <spacepepper3.14@gmail.com>
2024-08-12 15:51:06 -0400

    feature: pretty time text on slider

--- src/main/java/fast_reset/client/FastResetConfig.java
+++ src/main/java/fast_reset/client/FastResetConfig.java
@@ -1,13 +1,22 @@
 package fast_reset.client;
 
+import net.minecraft.client.gui.widget.SliderWidget;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import org.jetbrains.annotations.Nullable;
+import org.mcsr.speedrunapi.config.SpeedrunConfigAPI;
 import org.mcsr.speedrunapi.config.api.SpeedrunConfig;
-import org.mcsr.speedrunapi.config.api.annotations.Config;
+import org.mcsr.speedrunapi.config.api.SpeedrunOption;
+
+import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
 
 public class FastResetConfig implements SpeedrunConfig {
 
     public ButtonLocation buttonLocation = ButtonLocation.BOTTOM_RIGHT;
 
-    @Config.Numbers.Whole.Bounds(max = 60 * 5, enforce = Config.Numbers.EnforceBounds.MIN_ONLY)
     public int alwaysSaveAfter = 60 * 2 + 30;
 
     {
@@ -24,5 +33,49 @@ public class FastResetConfig implements SpeedrunConfig {
         CENTER,
         REPLACE_SQ,
         HIDE
+    }
+
+    private static class TimeSliderWidget extends SliderWidget {
+        private final SpeedrunOption<Integer> option;
+
+        public TimeSliderWidget(int x, int y, int width, int height, Text text, SpeedrunOption<Integer> option) {
+            super(x, y, width, height, text, (double) option.get() / (5 * 60));
+            this.option = option;
+            this.updateMessage();
+        }
+
+        @Override
+        protected void updateMessage() {
+            String baseKey = "speedrunapi.config." + option.getModID() + ".option." + option.getID();
+            int current = option.get();
+            int hours = current / (60 * 60);
+            LinkedHashMap<String, Integer> times = new LinkedHashMap<>(3);
+            times.put("hours", hours);
+            times.put("minutes", (current) % (60 * 60) / 60);
+            times.put("seconds", current % 60);
+            this.setMessage(times
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() > 0)
+                    .map(entry -> (MutableText) new TranslatableText(baseKey + "." + entry.getKey(), entry.getValue()))
+                    .reduce((text, text2) -> text.append(" ").append(text2)).orElse(new TranslatableText(baseKey + ".unknown"))
+            );
+        }
+
+        @Override
+        protected void applyValue() {
+            option.set((int) (this.value * 5 * 60));
+        }
+    }
+
+    @Override
+    public @Nullable SpeedrunOption<?> parseField(Field field, SpeedrunConfig config, String... idPrefix) {
+        Class<?> type = field.getType();
+        if (int.class.equals(type)) {
+            return new SpeedrunConfigAPI.CustomOption.Builder<Integer>(this, this, field, idPrefix)
+                    .createWidget((option, innerConfig, configStorage, optionField) -> new TimeSliderWidget(0, 0, 150, 20, LiteralText.EMPTY, option))
+                    .build();
+        }
+        return SpeedrunConfig.super.parseField(field, config, idPrefix);
     }
 }
